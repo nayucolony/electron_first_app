b7586efba0de0f931b3384cc6172e8515f05b4ab
nayucolony <nayu.fizzbot@gmail.com>
2017-05-14 01:31:42 +0900

    Setup firebase

--- electron_chat/src/renderer/app.jsx
+++ electron_chat/src/renderer/app.jsx
@@ -6,6 +6,9 @@ import Signup from "./Signup";
 import Rooms from "./Rooms";
 import Room from "./Room";
 
+import firebase from "firebase/firebase-browser";
+
+
 const appRouting = (
     <Router history={hashHistory}>
         <Route path="/">
@@ -21,6 +24,17 @@ const appRouting = (
 if(!location.hash.length){
     location.hash = "#/login";
 }
+
+const config = {
+    apiKey: "AIzaSyCtdtTOz5imTNbA3R-YS7IwYtuYgB5r9Eg",
+    authDomain: "electron-chat-7eeb7.firebaseapp.com",
+    databaseURL: "https://electron-chat-7eeb7.firebaseio.com",
+    projectId: "electron-chat-7eeb7",
+    storageBucket: "electron-chat-7eeb7.appspot.com",
+    messagingSenderId: "41782197380"
+};
+
+firebase.initializeApp(config);
 
 render(appRouting, document.getElementById("app"));
 
